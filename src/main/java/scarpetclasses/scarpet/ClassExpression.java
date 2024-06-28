ca006ac559d9fde8b5a96228f02919cdb43a49a5
Ghoulboy <ghoulboy27@gmail.com>
2024-06-28 12:46:39 +0200

    Adding infrastructure for adding many classvalue-based functions
    Without constantly having to cast, and check for stuff, etc.

--- src/main/java/scarpetclasses/scarpet/ClassExpression.java
+++ src/main/java/scarpetclasses/scarpet/ClassExpression.java
@@ -3,9 +3,13 @@ package scarpetclasses.scarpet;
 import carpet.script.CarpetExpression;
 import carpet.script.Expression;
 import carpet.script.exception.InternalExpressionException;
+import carpet.script.value.ListValue;
 import carpet.script.value.MapValue;
 import carpet.script.value.StringValue;
+import carpet.script.value.Value;
 import scarpetclasses.scarpet.value.ClassValue;
+
+import java.util.function.Function;
 
 //Temporary imports for testing purposes
 import static carpet.CarpetSettings.superSecretSetting;
@@ -14,22 +18,41 @@ import static carpet.CarpetServer.minecraft_server;
 
 public class ClassExpression {
 
-    public static void apply(CarpetExpression cexpr){
+    public static void apply(CarpetExpression cexpr) {
 
         Expression expr = cexpr.getExpr();
 
-        expr.addContextFunction("new_class", 2, (c, t, lv)->{
+        expr.addContextFunction("new_class", 2, (c, t, lv) -> {
             ClassValue newClass;
 
+            if (superSecretSetting) {
+                m(minecraft_server.getCommandSource(), "gi new class name: " + lv.get(0).getString());
+                m(minecraft_server.getCommandSource(), "gi new class params: " + lv.get(1).getString());
+            }
+
             if (lv.get(1) instanceof MapValue map)
-                 newClass = new ClassValue(lv.get(0).getString(), map.getMap());
+                newClass = new ClassValue(lv.get(0).getString(), map.getMap());
             else
                 throw new InternalExpressionException("Must declare a class with a map of fields and methods.");
 
             return newClass;
         });
 
-        expr.addUnaryFunction("class_name", v-> StringValue.of(((ClassValue) v).className));
+        addUnaryClassFunction(expr, "class_name", c -> StringValue.of(c.className));
+        addUnaryClassFunction(expr, "class_fields", c -> ListValue.wrap(c.fields.keySet().stream().map(StringValue::of)));
+        addUnaryClassFunction(expr, "class_methods", c -> ListValue.wrap(c.methods.keySet().stream().map(StringValue::of)));
 
+    }
+
+    /**
+     * Simple way of adding classvalue based functions, since there are plenty of these
+     */
+    public static void addUnaryClassFunction(Expression expr, String name, Function<ClassValue, Value> fun) {
+        expr.addUnaryFunction(name, v -> {
+            if (v instanceof ClassValue c) {
+                return fun.apply(c);
+            }
+            throw new InternalExpressionException(name + " requires a class value as argument, not " + v.getTypeString());
+        });
     }
 }
