367207bc0eee2fa04d74c973c44863c1cd4acc89
Ghoulboy <ghoulboy27@gmail.com>
2024-06-28 11:55:08 +0200

    Optimising imports
    Also preparing for removing .gitignore files

--- src/main/java/scarpetclasses/scarpet/ClassExpression.java
+++ src/main/java/scarpetclasses/scarpet/ClassExpression.java
@@ -3,14 +3,13 @@ package scarpetclasses.scarpet;
 import carpet.CarpetServer;
 import carpet.script.CarpetExpression;
 import carpet.script.Expression;
-import carpet.script.ScriptHost;
-import carpet.script.Tokenizer;
 import carpet.script.exception.InternalExpressionException;
 import carpet.script.value.MapValue;
 import carpet.script.value.StringValue;
-import carpet.script.value.Value;
-import carpet.utils.Messenger;
 import scarpetclasses.scarpet.value.ClassValue;
+
+import static carpet.CarpetSettings.superSecretSetting;
+import static carpet.utils.Messenger.m;
 
 public class ClassExpression {
     public static void carpetApply(CarpetExpression cexpr){
@@ -20,20 +19,14 @@ public class ClassExpression {
     public static void apply(Expression expr){
         expr.addContextFunction("new_class", 2, (c, t, lv)->{
             ClassValue newClass;
-            Messenger.m(CarpetServer.minecraft_server.getCommandSource(), "gi Ran new_class function with name "+ lv.get(0).getString());
-            Messenger.m(CarpetServer.minecraft_server.getCommandSource(), "gi Param map "+ lv.get(1).getString());
 
             if (lv.get(1) instanceof MapValue map)
                  newClass = new ClassValue(lv.get(0).getString(), map.getMap());
             else
                 throw new InternalExpressionException("Must declare a class with a map of fields and methods.");
 
-            Messenger.m(CarpetServer.minecraft_server.getCommandSource(), "gi Created new class");
-
-
-            return Value.NULL;
+            return newClass;
         });
-
 
         expr.addUnaryFunction("class_name", v-> StringValue.of(((ClassValue) v).className));
 
